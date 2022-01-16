b8a46f54992e5df047e8ad4da74425af6850291e
Nic Scobey <nicscobey@gmail.com>
2022-01-15 21:37:43 -0800

    updated blog route

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/blog">
+        <Route path="/blog">
           <Blog />
         </Route>
       </Switch>

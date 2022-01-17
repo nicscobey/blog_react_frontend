5b8f87913d395da2be3ff50a86001c6ddb5becd3
Nic Scobey <nicscobey@gmail.com>
2022-01-17 09:05:57 -0800

    added link

--- src/components/blogCardLarge.jsx
+++ src/components/blogCardLarge.jsx
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom '
+
 const BlogCardLarge = () => {
 
     // theme border will likely need to be written in JS to make the color dynamic based on theme
 
     const CreateCards = () => {
         return (
-            <a href="/blog" className="color-black">
+            <Link to="/blog" className="color-black">
                 <div className="blog-card-large">
                     <img className="card-image-large" src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
                     <div className="large-card-bottom">
@@ -14,7 +16,7 @@ const BlogCardLarge = () => {
                         <p className="large-card-published">Firstname Lastname * ##/##/####</p>
                     </div> 
                 </div>
-            </a>
+            </Link>
             
         )
     }
